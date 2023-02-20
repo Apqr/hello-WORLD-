3d7c5506f92e7130b71c2c7814c86c731d7b26fc
Apqr <93971288+Apqr@users.noreply.github.com>
2023-02-20 22:31:30 +0530

    Create JSON.js
    For APIs

---
+++ JSON.js
@@ -0,0 +1,26 @@
+[
+  {
+    "name":"Satoshi Hirano",
+    "img":"https://blob.sololearn.com/avatars/d38a0398-2e32-4a54-a249-b263caa8566e.jpg",
+    "country":"Japan",
+    "level":20,
+    "designation":"Pro",
+    "profile":"https://www.sololearn.com/profile/8091773"
+  },
+   {
+    "name":"Hari Sk",
+    "img":"https://blob.sololearn.com/avatars/f68294be-387a-410f-8d35-a4b74057da89.jpg",
+    "country":"India",
+    "level":6,
+    "designation":"Student",
+    "profile":"https://www.sololearn.com/profile/23264126"
+  },
+  {
+    "name":"Jayakrishna 🇮🇳",
+    "img":"https://blob.sololearn.com/avatars/35e252ba-bf44-41ac-a528-55216bf22b7f.jpg",
+    "country":"India",
+    "level":16,
+    "designation":"Mentor",
+    "profile":"https://www.sololearn.com/profile/12469228"
+  }
+]
